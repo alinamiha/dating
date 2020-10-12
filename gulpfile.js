df6abbbea92f29511b5a040ab8d489f787cc6c98
Alina <alinamiha293@gmail.com>
2020-10-12 20:31:48 +0300

    Fix bugs

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 let gulp = require('gulp');
 let sass = require('gulp-sass');
+let sourcemaps = require('gulp-sourcemaps');
+let watch = require('gulp-watch');
 
 gulp.task('sass', function (done) {
     gulp.src('./src/sass/**/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
+	.pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./main/styles'))
     done()
 })
